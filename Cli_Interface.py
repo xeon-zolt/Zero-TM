12650a27846eb439c3e9ab2472aadf6d9575d1c0
Harsh Lathwal <xeon.harsh@gmail.com>
2017-02-26 22:07:44 +0530

    hmmm

--- Cli_Interface.py
+++ Cli_Interface.py
@@ -1,2 +1,8 @@
 from Detect_De import * # to detect Desktop Enviourment
 from get_themes import *
+
+for i in range(len(sort_themes_by_de())):
+    print (str(i+1)+' : '+sort_themes_by_de()[i])
+theme_number = int(input('To Apply Theme enter the Theme Number like 1 or 2 etc'))
+apply(theme_number)
+# add error handling here
